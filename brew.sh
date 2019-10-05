54f41d89eabfe9aa28388e7c6ea5ea71d243e464
Alexander Kiselev <a@kslv.me>
2019-10-05 12:30:01 +0300

    init

---
+++ brew.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+if [[ -z "$(which brew)" ]]
+  then
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+brew tap caskroom/cask
+brew tap buo/cask-upgrade
+
+brew install ansible \
+dnscontrol \
+git \
+go \
+iperf3 \
+mas \
+mtr \
+nmap \
+python \
+terraform \
+tmux \
+wget \
+zsh \
+zsh-autosuggestions \
+zsh-syntax-highlighting
+
+brew cask install 1password \
+#appcleaner \
+#daisydisk \
+firefox \
+iterm2 \
+nrlquaker-winbox \
+teamviewer \
+transmission \
+viscosity \
+visual-studio-code \
+vlc
+#vmware-fusion
+
+mas install 1153157709 # Speedtest
+mas install 1440147259 # AdGuard for Safari
+mas install 1451685025 # WireGuard
+mas install 425424353 # The Unarchiver
+mas install 524141863 # Jump Desktop
+mas install 747648890 # Telegram
