af6663ad80b0421ab3fa7f25c88eed775ae71813
Alexander Kiselev <a@kslv.me>
2024-04-26 21:47:57 +0300

    Add hashicorp/tap

--- brew.sh
+++ brew.sh
@@ -1,35 +1,36 @@
 #!/bin/bash
 
-if [[ -z "$(which brew)" ]]
-  then
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+if [[ -z "$(which brew)" ]]; then
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
 #brew tap homebrew/cask-cask
 brew tap buo/cask-upgrade
+brew tap hashicorp/tap
 
 brew install ansible \
-antigen \
-dnscontrol \
-git \
-iperf3 \
-mas \
-mtr \
-nmap \
-wget \
-zsh-autosuggestions \
-zsh-syntax-highlighting
-
+  antigen \
+  dnscontrol \
+  git \
+  iperf3 \
+  mas \
+  mtr \
+  nmap \
+  wget \
+  zsh-autosuggestions \
+  zsh-syntax-highlighting \
+  hashicorp/tap/packer \
+  hashicorp/tap/terraform
 
 # cask
 brew install --cask 1password \
-anydesk \
-appcleaner \
-nrlquaker-winbox \
-transmission \
-viscosity \
-visual-studio-code \
-zoom
+  anydesk \
+  appcleaner \
+  nrlquaker-winbox \
+  transmission \
+  viscosity \
+  visual-studio-code \
+  zoom
 #iina
 #iterm2
 
@@ -39,6 +40,6 @@ brew cu pin nrlquaker-winbox
 # mas-cli
 mas install 1440147259 # AdGuard for Safari
 mas install 1451685025 # WireGuard
-mas install 524141863 # Jump Desktop
-mas install 747648890 # Telegram
+mas install 524141863  # Jump Desktop
+mas install 747648890  # Telegram
 mas install 1474335294 # GoodLinks
